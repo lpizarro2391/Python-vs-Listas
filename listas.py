114df3bf0b127c59b60264ed2097dd1ef6895db7
lpizarro2391 <lourdespizarro@gmail.com>
2020-04-27 18:51:35 -0600

    Escribe un programa en Python que registre el nombre de cinco criptomonedas, con sus respectivas cantidades y precios en USD, haciendo uso de listas. Luego, imprima los datos de cada moneda una vez que han sido ingresadas por el usuario.
    Recuerda que debes hacer uso de las sentencias de lectura y escritura para que el usuario indique los datos a registrar.

--- listas.py
+++ listas.py
@@ -1,1 +1,14 @@
-
+i = 0
+cripto = []
+cant = []
+cotiz = []
+while i < 5:
+    cripto.append(input("Indique el nombre dela criptomoneda : "))
+    cant.append(float(input("Indique la cantidad de "+ cripto[i]+": ")))
+    cotiz.append(
+        float(input("Indique la cotizacion en USD de "+cripto[i]+": ")))
+    i=i+1
+i=0
+while i<5:
+    print("Moneda: ",cripto[i],"Cantidad: ",str(cant[i]),",Cotizacion: "+str(cotiz[i]))
+    i=1+1
