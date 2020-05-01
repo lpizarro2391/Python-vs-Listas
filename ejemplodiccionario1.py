227258b74a7a381dce72bb32b11150e2304a6102
lpizarro2391 <lourdespizarro@gmail.com>
2020-05-01 12:49:04 -0600

    OPCION CAMBIANDO EL API

--- ejemplodiccionario1.py
+++ ejemplodiccionario1.py
@@ -1,22 +1,31 @@
 import requests
 
+
 def esmoneda(cripto):
     return cripto in monedas
 
-monedas=()
-monedas_dict={}
 
-data=requests.get("http://api.coinmarketcap.com/v2/ticker/").json()
+monedas = ()
+monedas_dict = {}
+
+COINMARKET_API_KEY = "7dc3eb05-b9ca-401e-835e-1eaceaef62cf"
+headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': '7dc3eb05-b9ca-401e-835e-1eaceaef62cf'
+}
+data = requests.get(
+    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest", headers=headers).json()
 for id in data["data"]:
-    monedas_dict[data["data"][id]["symbol"]]=data["data"][id]["quotes"]["USD"]["Price"]
+    monedas_dict[data["data"][id]["symbol"]
+                 ] = data["data"][id]["quotes"]["USD"]["Price"]
 
-monedas=monedas_dict.keys()
+monedas = monedas_dict.keys()
 
-moneda=input("Indique el nombre de la moneda a obtener el precio")
+moneda = input("Indique el nombre de la moneda a obtener el precio")
 while not esmoneda(moneda):
-        print("Moneda Inválida")
-        moneda=input("Ingrese el nombre de la moneda")
+    print("Moneda Inválida")
+    moneda = input("Ingrese el nombre de la moneda")
 
 else:
-    print("La moneda con symbol:",moneda,
-         "tiene un precio de : ",monedas_dict.get(moneda),"USD")
+    print("La moneda con symbol:", moneda,
+          "tiene un precio de : ", monedas_dict.get(moneda), "USD")
