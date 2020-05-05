44e90a4c8e1484443db9ab0d0545ac78d68cd695
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-05 09:52:04 -0600

    se ejecuta desde la consola, se accede poniendo cd y la ruta, se escrible py ejercicio.py y dá el resultado del código

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,4 @@
 from Criptomonedas import Criptomoneda
-ethereum = Criptomoneda ("ETH ", 0.0, 454.543)
+ethereum = Criptomoneda ("ETH ", 1.0, 454.543)
 print(ethereum.mostrarNombre(),ethereum.calcularSaldo("USD"))
 
---
+++ ejercicio1tallerclasesyobjetos.py
@@ -0,0 +1,6 @@
+from Criptomonedas import Criptomoneda
+bitcoin = Criptomoneda ("BTC", 0.34, 5000.00)
+bitcoin.mostrarNombre()
+bitcoin.calcularSaldo("USD")
+bitcoin.indicarSaldo(0.5)
+bitcoin.calcularSaldo("USD")
