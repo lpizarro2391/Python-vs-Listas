1409cfe83467bdd4e761c188665858b98b1352e1
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-05 12:06:28 -0600

    listo, se ejecuta desde CMD O TERMINAL

--- ejercicio1tallerclasesyobjetos.py
+++ ejercicio1tallerclasesyobjetos.py
@@ -1,7 +1,7 @@
 from Criptomonedas import Criptomoneda
 bitcoin = Criptomoneda ("BTC", 0.34, 5000.00)
 print (bitcoin.mostrarNombre())
-bitcoin.calcularSaldo("USD")
+print (bitcoin.calcularSaldo("USD"))
 bitcoin.indicarSaldo(0.5)
 bitcoin.calcularSaldo("USD")
-print (bitcoin.mostrarSaldo)
+print (bitcoin.calcularSaldo("USD"))
