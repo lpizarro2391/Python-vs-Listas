90d442219438783c9a7bbf09f824f5d694be5362
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-05 12:18:43 -0600

    ok se ejecuta desde CMD

---
+++ ejer3tallerclasesyobjetos.py
@@ -0,0 +1,9 @@
+from Criptomonedas import Criptomoneda
+ripple = Criptomoneda ("XRP",34.93,0.4785)
+ripple.mostrarNombre()
+print (ripple.mostrarNombre())
+ripple.indicarNombre("XRP")
+ripple.mostrarNombre()
+print (ripple.mostrarNombre())
+ripple.calcularSaldo("USD")
+print (ripple.calcularSaldo("USD"))
